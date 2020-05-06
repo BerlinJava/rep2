718ae5b9c81091a5f2dc0356460134caaeb8ff33
berlin <1075967996@qq.com>
2020-05-06 11:41:19 +0800

    three---77777

--- Hello01/src/com/berlin/test/HelloWorld.java
+++ Hello01/src/com/berlin/test/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.berlin.test;
 
 public class HelloWorld {
-    //6666666
+    //6666666777777777777777
     public static void main(String[] args) {
         System.out.println("hello");
     }
