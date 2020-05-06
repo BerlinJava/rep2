daf91e4004f031ee59b01097935b0acbc7d451a2
berlin <1075967996@qq.com>
2020-05-06 12:01:10 +0800

    four---9999

--- Hello01/src/com/berlin/test/HelloWorld.java
+++ Hello01/src/com/berlin/test/HelloWorld.java
@@ -1,8 +1,9 @@
 package com.berlin.test;
 
 public class HelloWorld {
-    //6666666777777777777777
+    //66666669999
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("我下班了");
     }
 }
